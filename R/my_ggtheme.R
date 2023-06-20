fd1abe14c9041453c75099b0e6a4ec49277cee28
Julie Ballard <bronnimannj@gmail.com>
2023-06-20 16:27:10 +0100

    add import ggplot each time function is called

--- R/my_ggtheme.R
+++ R/my_ggtheme.R
@@ -9,22 +9,22 @@
 #' @export
 #'
 #' @examples
-#' ggplot(data.frame(a = c(1,2),b = c(3,4)), aes(a,b)) + my_ggtheme()
+#' ggplot2::ggplot(data.frame(a = c(1,2),b = c(3,4)), ggplot2::aes(a,b)) + my_ggtheme()
 my_ggtheme <- function(x_percent = FALSE,
                        y_percent = FALSE){
 
-  p1 <- theme_light() +
-    theme(plot.title = element_text(hjust = 0.5,
+  p1 <- ggplot2::theme_light() +
+    ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5,
                                     face = "bold"))
 
   if(x_percent){
     p1 <- p1 +
-      scale_x_continuous(labels = scales::percent)
+      ggplot2::scale_x_continuous(labels = scales::percent)
   }
 
   if(y_percent){
     p1 <- p1 +
-      scale_y_continuous(labels = scales::percent)
+      ggplot2::scale_y_continuous(labels = scales::percent)
   }
 
   p1
